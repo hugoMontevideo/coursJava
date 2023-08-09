1a5e108dc6c5a9f3b1be9ba0b55b5d0b02c27e3d
hugoMontevideo <machadopereira.hugo@gmail.com>
2023-08-09 22:56:18 +0200

    first java commit

---
+++ class_objet/src/VehiculeAMoteur.java
@@ -0,0 +1,11 @@
+public class VehiculeAMoteur {
+    private Moteur moteur;
+
+    public VehiculeAMoteur(){
+
+    }
+    VehiculeAMoteur(Moteur mot){
+        moteur = mot; // this n'est pas obligatoire tant qu'il n'y a pas de propriété possédant la meme nom dans des variables en argument
+    }
+
+}
